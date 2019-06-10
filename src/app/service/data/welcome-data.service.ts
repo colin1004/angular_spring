b8e73e9a2dd548dcad18082c05a3ea3fa47ce013
jq1004 <junqingl@usc.edu>
2019-06-10 11:08:49 -0400

    create data service in angualr

---
+++ src/app/service/data/welcome-data.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export class WelcomeDataService {
+  constructor() {}
+  executeHelloWorldBeanService() {
+    console.log("Execute Hellow World Bean Service");
+  }
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { WelcomeDataService } from "../service/data/welcome-data.service";
 // import { AppComponent } from "../app.component";
 
 @Component({
@@ -11,11 +12,19 @@ export class WelcomeComponent implements OnInit {
   message = "some welcome message";
   name = "";
   // dependency injection
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService
+  ) {}
 
   ngOnInit() {
     console.log(this.message);
     // console.log(this.route.snapshot.params["name"]);
     this.name = this.route.snapshot.params["name"];
+  }
+
+  getWelcomeMessage() {
+    this.service.executeHelloWorldBeanService();
+    // console.log("get welcome message");
   }
 }
