ef2178025d3abf12669164db5fbf5c59d7250cba
jq1004 <junqingl@usc.edu>
2019-06-10 08:32:16 -0400

    secure components using route guards

--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -6,10 +6,10 @@ import { Injectable } from "@angular/core";
 export class HardcodedAuthenticationService {
   constructor() {}
   authenticate(username, password) {
-    console.log("before " + this.isUserLoggedIn());
+    // console.log("before " + this.isUserLoggedIn());
     if (username == "asdf" && password == "123456") {
       sessionStorage.setItem("authenticateUser", username);
-      console.log("after " + this.isUserLoggedIn);
+      // console.log("after " + this.isUserLoggedIn);
       return true;
     }
     return false;
@@ -18,5 +18,9 @@ export class HardcodedAuthenticationService {
   isUserLoggedIn() {
     let user = sessionStorage.getItem("authenticateUser");
     return !(user === null);
+  }
+
+  logout() {
+    sessionStorage.removeItem("authenticateUser");
   }
 }
